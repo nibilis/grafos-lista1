058be901bfce0b5d8bccc12100ba93badf9fdeac
nibilis <nhenriquesdealmeida@gmail.com>
2025-08-30 18:04:15 -0300

    Change variables names and remove comments

--- src/models/directed_matrix_graph.py
+++ src/models/directed_matrix_graph.py
@@ -1,12 +1,10 @@
 import os
 
 class DirectedMatrixGraph:
-    # construtor da classe grafo
-    def __init__(self, n = 0):
-        self.n = n # número de vértices
-        self.m = 0 # número de arestas
-        # matriz de adjacência
-        self.adj = [[0 for i in range(n)] for j in range(n)]
+    def __init__(self, nodes = 0):
+        self.nodes = nodes
+        self.edges = 0
+        self.adjacency_matrix = [[0 for i in range(nodes)] for j in range(nodes)]
 
     # Exercicio 7
     def graph_from_file(self, filename):
@@ -21,7 +19,7 @@ class DirectedMatrixGraph:
 
             edges = int(file.readline())
             if edges == 0:
-                self.n = nodes
+                self.nodes = nodes
                 return self
 
             if edges > nodes ** 2:
@@ -39,35 +37,35 @@ class DirectedMatrixGraph:
                     return None
                 adj_matrix[origin][destiny] = 1
 
-            self.n = nodes
-            self.m = edges
-            self.adj = adj_matrix
+            self.nodes = nodes
+            self.edges = edges
+            self.adjacency_matrix = adj_matrix
             return self
                     
 
 	# Insere uma aresta no Grafo tal que
 	# v é adjacente a w
     def insert_a(self, v, w):
-        if self.adj[v][w] == 0:
-            self.adj[v][w] = 1
-            self.m+=1 # atualiza qtd arestas
+        if self.adjacency_matrix[v][w] == 0:
+            self.adjacency_matrix[v][w] = 1
+            self.edges+=1 # atualiza qtd arestas
     
     # remove uma aresta v->w do Grafo	
     def remove_a(self, v, w):
         # testa se temos a aresta
-        if self.adj[v][w] == 1:
-            self.adj[v][w] = 0
-            self.m-=1
+        if self.adjacency_matrix[v][w] == 1:
+            self.adjacency_matrix[v][w] = 0
+            self.edges-=1
 
 	# Apresenta o Grafo contendo
 	# número de vértices, arestas
 	# e a matriz de adjacência obtida	
     def show(self):
-        print(f"\n n: {self.n:2d} ", end="")
-        print(f"m: {self.m:2d}\n")
-        for i in range(self.n):
-            for w in range(self.n):
-                if self.adj[i][w] == 1:
+        print(f"\n n: {self.nodes:2d} ", end="")
+        print(f"m: {self.edges:2d}\n")
+        for i in range(self.nodes):
+            for w in range(self.nodes):
+                if self.adjacency_matrix[i][w] == 1:
                     print(f"Adj[{i:2d},{w:2d}] = 1 ", end="") 
                 else:
                     print(f"Adj[{i:2d},{w:2d}] = 0 ", end="")
@@ -80,11 +78,11 @@ class DirectedMatrixGraph:
 	# e a matriz de adjacência obtida 
     # Apresentando apenas os valores 0 ou 1	
     def show_min(self):
-        print(f"\n n: {self.n:2d} ", end="")
-        print(f"m: {self.m:2d}\n")
-        for i in range(self.n):
-            for w in range(self.n):
-                if self.adj[i][w] == 1:
+        print(f"\n n: {self.nodes:2d} ", end="")
+        print(f"m: {self.edges:2d}\n")
+        for i in range(self.nodes):
+            for w in range(self.nodes):
+                if self.adjacency_matrix[i][w] == 1:
                     print(" 1 ", end="") 
                 else:
                     print(" 0 ", end="")
@@ -95,16 +93,16 @@ class DirectedMatrixGraph:
     # Exercicio 1
     def in_degree(self, v):
         in_degree = 0
-        for aresta in range(self.n):
-            if self.adj[aresta][v] == 1:
+        for aresta in range(self.nodes):
+            if self.adjacency_matrix[aresta][v] == 1:
                 in_degree += 1
         return in_degree
     
     # Exercicio 2
     def out_degree(self, v):
         out_degree = 0
-        for aresta in range(self.n):
-            if self.adj[v][aresta] == 1:
+        for aresta in range(self.nodes):
+            if self.adjacency_matrix[v][aresta] == 1:
                 out_degree += 1
         return out_degree
     
@@ -126,9 +124,9 @@ class DirectedMatrixGraph:
     
     # Exercicio 6
     def is_simetric(self):
-        for i in range(self.n):
-            for j in range(self.n - i):
-                if self.adj[i][j] != self.adj[j][i]:
+        for i in range(self.nodes):
+            for j in range(self.nodes - i):
+                if self.adjacency_matrix[i][j] != self.adjacency_matrix[j][i]:
                     return False
         return True
     
