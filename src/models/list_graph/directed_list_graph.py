1dd0e45da67166755bc838f72f004569fb358dc1
Akuro <marcocamargoes@gmail.com>
2025-09-07 21:55:12 -0300

    Arrumando funcao de ler arquivo

--- src/models/list_graph/directed_list_graph.py
+++ src/models/list_graph/directed_list_graph.py
@@ -44,7 +44,7 @@ class DirectedListGraph(ListGraph):
     # Exercicio 28
     def graphFromFile(self, filename):
         base_path = os.path.dirname(__file__)
-        full_path = os.path.join(base_path, "..", "..", "tests/resources", filename)
+        full_path = os.path.join(base_path, "..", "..", "..", "tests/resources", filename)
         full_path = os.path.abspath(full_path)
 
         with open(full_path, "r") as file:
--- src/models/matrix_graph/directed_matrix_graph.py
+++ src/models/matrix_graph/directed_matrix_graph.py
@@ -9,7 +9,7 @@ class DirectedMatrixGraph(MatrixGraph):
     # Exercicio 7
     def graph_from_file(self, filename):
         base_path = os.path.dirname(__file__)
-        full_path = os.path.join(base_path, "..", "..", "tests/resources", filename)
+        full_path = os.path.join(base_path, "..", "..", "..", "tests/resources", filename)
         full_path = os.path.abspath(full_path)
 
         with open(full_path, "r") as file:
